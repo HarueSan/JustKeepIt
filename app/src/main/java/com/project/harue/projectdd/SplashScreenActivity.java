232f2b0ec4c63c61c6be8d553d13e8fa3ef23f49
harue <haruethai_sangyod@elearning.cmu.ac.th>
2019-04-22 22:08:43 +0700

    -I'm doneeeee

--- app/src/main/java/com/project/harue/projectdd/SplashScreenActivity.java
+++ app/src/main/java/com/project/harue/projectdd/SplashScreenActivity.java
@@ -1,4 +1,33 @@
+
 package com.project.harue.projectdd;
 
-public class SplashScreenActivity {
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.os.Handler;
+import android.support.v7.app.AppCompatActivity;
+
+public class SplashScreenActivity extends AppCompatActivity {
+
+    private static int splashTime = 3000;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.splash);
+        getSupportActionBar().hide();
+
+        new Handler().postDelayed(new Runnable() {
+            @Override
+            public void run() {
+
+                Intent mySuperIntent = new Intent(SplashScreenActivity.this, LoginActivity.class).setFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP);
+                startActivity(mySuperIntent);
+                finish();
+            }
+        }, splashTime);
+
+    }
+
+
 }
